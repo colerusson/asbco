ed27e0bdba1d8d255992e5c3c02928bf08b57e49
Alex Mercer <a.mercer118@gmail.com>
2024-03-04 12:40:34 -0700

    Added login page using Quincy's great work

---
+++ src/login/login.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import FormCard from '../register/FormCard'
+import TextInput from '../register/TextInput'
+import MainTitle from '../register/MainTitle'
+import Button from '../register/Button'
+import '../register/register.css'
+
+const Login = ({ presenter }) => {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+
+	const style = {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		width: '100vw'
+	}
+
+	return (
+		<div className='login' style={style}>
+			<MainTitle text="Anti-Scroll" />
+			<FormCard>
+				<TextInput
+					label="Username"
+					placeholder=""
+					onChange={(input) => setUsername(input.target.value)}
+				/>
+				<TextInput
+					label="Password"
+					placeholder=""
+					onChange={(input) => setPassword(input.target.value)}
+				/>
+				<Button
+					text="Login"
+					variant="primary"
+					onClick={() => { presenter.login(username, password) }}
+				/>
+				<Button
+					text="Register"
+					variant="secondary"
+					onClick={() => { window.location.href = '/register' }}
+				/>
+			</FormCard>
+		</div>
+	)
+}
+
+export default Login
